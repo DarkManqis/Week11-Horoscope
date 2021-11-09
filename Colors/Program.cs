d0db182ffc2075abf419f48d89691d6a4d1b2d45
Opilane TTHK <opilane@tthk.ee>
2021-11-09 15:55:11 +0200

    horoscope

---
+++ Colors/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            string userSign = Console.ReadLine(); 
+
+            Console.WriteLine(GetColors(userSign));
+            
+        }
+
+        
+
+        public static string[] ReadColors()
+        {
+
+            string filePath = @"C:\Users\opilane\samples\personality";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            return dataFromFile;
+
+        }
+
+        public static string GetColors(string userInput)
+        {
+            string[] Colors = ReadColors();
+            string todayColors = "";
+
+            foreach (string element in Colors)
+            {
+                if (element.Contains(userInput))
+                {
+                    todayColors = element;
+
+                }
+            }
+            return todayColors;
+        }
+
+
+
+    }
+}
---
+++ Personality/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Personality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
